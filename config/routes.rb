7f6d029a44cd01c375fc95d0f84ff95aa7b66bf2
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-17 19:41:46 -0600

    Add User
    A user can be created.
    Guest can enter new user_path, create account and be sent to their specific show page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index, :show]
 
+  resources :users, only: [:new, :create, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415212116) do
+ActiveRecord::Schema.define(version: 20160418000834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20160415212116) do
   end
 
   add_index "ratings", ["book_id"], name: "index_ratings_on_book_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "ratings", "books"
 end
