01954f7e68d0d14da4c11aed33b8ea6c59690893
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-19 14:32:35 -0600

    Adding model test for book#average_score
    Average now returns as a float
    When there are no ratings nil will return as float 0.0
    Took advantage of the big decimal round method

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it "has an average_rating" do
-    book = Book.create!(title: "The Hobbit", author: "JRR Tolkin", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+  context "derived attribute" do
+    describe '#average_rating' do
+      it "has an average_rating" do
+        book = Book.create!(title: "The Hobbit", author: "JRR Tolkin", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
 
-    rating1 = Rating.create!(score: 2, book_id: book.id)
-    rating2 = Rating.create!(score: 4, book_id: book.id)
-    rating3 = Rating.create!(score: 3, book_id: book.id)
+        rating3 = Rating.create!(score: 3, book_id: book.id)
+        rating1 = Rating.create!(score: 2, book_id: book.id)
+        rating2 = Rating.create!(score: 4, book_id: book.id)
 
-    expect(book.average_rating).to eq 3
+        expect(book.average_rating).to be 3.0
+      end
+
+      it "rounds to the nearest 10th when average becomes a float" do
+        book = Book.create!(title: "The Hobbit", author: "JRR Tolkin", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+
+        rating1 = Rating.create!(score: 2, book_id: book.id)
+        rating2 = Rating.create!(score: 2, book_id: book.id)
+        rating3 = Rating.create!(score: 3, book_id: book.id)
+        
+        expect(book.average_rating).to be 2.3
+      end
+    end
   end
 end
