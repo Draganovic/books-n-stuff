61ab757ec76d2caf7b9a912c89713181a7ef44c3
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-22 09:42:43 -0600

    Add admin role level
    Create admin dashboard namespace
    Only admin can add a book

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,11 @@ class SessionsController < ApplicationController
         # @user = User.find(session[])
         # set the current session's user_id tp this @user
         session[:user_id] = @user.id
-        redirect_to user_path(@user)
+        if @user.admin?
+          redirect_to admin_dashboard_path
+        else
+          redirect_to user_path(@user)
+        end
       else
         flash.now[:error] = "Invalid Login Info"
         render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
 
-  resources :books, only: [:index, :show] do
+  resources :books, only: [:index, :show, :new, :create] do
     resources :ratings, only: [:new, :create]
   end
   resources :users, only: [:new, :create, :show]
+
+  namespace :admin do
+    resource :dashboard, only: [:show]
+  end
 
 
   get "login", to: "sessions#new"
