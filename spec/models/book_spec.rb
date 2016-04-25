3ace648c0a56b9b8c5667fe5d9e29e1cbaf8ad61
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-25 15:26:37 -0600

    Add model level test for alphabetical order of books

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -22,6 +22,14 @@ RSpec.describe Book, type: :model do
 
         expect(book.average_rating).to be 2.3
       end
+
+      it "displays books in order" do
+        book1 = Book.create!(title: "c", author: "c author", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+        book2 = Book.create!(title: "a", author: "a author", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+        book3 = Book.create!(title: "b", author: "b author", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+
+        expect(Book.alphabetize).to eq [book2, book3, book1]
+      end
     end
   end
 end
