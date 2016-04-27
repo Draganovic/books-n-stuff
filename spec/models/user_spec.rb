98b78457669ceab658c387f0997858fd38777d79
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-27 13:08:17 -0600

    Final

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with username and password" do
+    expect(User.new(username: "Hank", password: "password")).to be_valid
+  end
+
+  it "is valid with username, password and role" do
+    expect(User.new(username: "Hank", password: "password", role: 1)).to be_valid
+  end
 end
