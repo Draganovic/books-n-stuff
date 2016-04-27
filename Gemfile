a163d3ea29a03a46561e490046d798671bbb003f
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-27 12:46:41 -0600

    Add scoping to image link test

--- Gemfile
+++ Gemfile
@@ -21,11 +21,13 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bcrypt'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
-# Use Unicorn as the app server
+#
+# # Use Unicorn as the app server
 # gem 'unicorn'
 
 # Use Capistrano for deployment
@@ -48,6 +50,5 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-  gem 'bcrypt'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 end
--- spec/features/guest_can_view_specific_book_spec.rb
+++ spec/features/guest_can_view_specific_book_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.feature 'guest can view all books' do
   scenario 'they view every book in the database' do
-    book1 = Book.create!(title: "The Hobbit", author: "JRR Tolkin", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
-    book2 = Book.create!(title: "A Brave New World", author: "Aldus Huxley", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
+    book = Book.create!(title: "The Hobbit", author: "JRR Tolkin", image: "http://vignette3.wikia.nocookie.net/lotr/images/b/b8/Hobbit-cover.jpg/revision/latest?cb=20070706183816")
 
     visit books_path
-    first("a").click
-    expect(current_path).to eq book_path(book2)
+
+    within(".books") do
+      first("a").click
+      expect(current_path).to eq book_path(book)
+    end
 
     within 'h1#title' do
-      expect(page).to have_content book2.title
+      expect(page).to have_content book.title
     end
     within '.book_info' do
-      expect(page).to have_content book2.author
-      expect(page).to have_content book2.average_rating
+      expect(page).to have_content book.author
+      expect(page).to have_content book.average_rating
     end
   end
 end
